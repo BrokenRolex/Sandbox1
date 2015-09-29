33e99b0006d338f2f3a43d4e5504001bd525ab68
Erick Nelson <ericksn@gmail.com>
2015-09-29 14:44:37 -0700

    use pure groovy , no dom4j

--- groovy-script-tools/src/main/groovy/script/xml/Combiner.groovy
+++ groovy-script-tools/src/main/groovy/script/xml/Combiner.groovy
@@ -1,132 +1,47 @@
 package script.xml
 
-import org.dom4j.*
-import org.dom4j.io.*
+import groovy.xml.*
 
 @groovy.util.logging.Log4j
 class Combiner {
-    private static final String ENCODING = 'UTF-8'
 
+    Node root
+    NodeList children
+    String encoding = 'UTF-8'
     String dtd
-    String encoding
-    List files = []
-    int size = 0
-    int added = 0 // number of documents added
-    boolean overwrite
-    private Document document
-    private Element root
-
-    void add (String xml) {
-        if (xml == null) {
-            throw new Exception("xml string is null")
-        }
-        addDoc(DocumentHelper.parseText(xml))
-        size += xml.length()
-    }
 
     void add (File file) {
-        if (file == null) {
-            throw new Exception("cannot add a null file object")
-        }
-        if (!file.isFile()) {
-            throw new Exception("cannot add [$file] because it does not exist")
-        }
-        log.debug "adding [$file] size [${file.size()}]"
-        //addDoc(new SAXReader().read(new BufferedInputStream(new FileInputStream(file))))
-        addDoc(new SAXReader().read(file))
-        size += file.size()
-        files << file
-    }
-
-    private void addDoc (Document doc) {
-        if (doc == null) {
-            throw new Exception("cannot add null document")
-        }
-        if (document == null) { // first document ?
-            document = doc
-            if (encoding == null) {
-                encoding = document.getXMLEncoding()
-            }
-            root = document.getRootElement()
-            if (dtd != null) {
-                document.addDocType(root.name, null, dtd)
-            }
-            return
-        }
-
-        // subsequent documents...
-
-        Document subdoc = doc
-        Element subroot = subdoc.getRootElement()
-
-        // add content
-        if (root.name != subroot.name) {
-            log.warn "document root [${root.name}] not the same as added document root [${subroot.name}]"
-        }
-        subroot.elements().each { root.add(it.detach()) }
-
-        added++
-    }
-
-    /**
-     * Write combined xml document to a file
-     * @param file to write xml to
-     * @param filter optional Visitor object to apply before writing
-     */
-    void write (File file, Visitor visitor = null) {
-        if (file == null) {
-            throw new Exception("cannot write to a null file object")
-        }
-        if (file.exists()) {
-            if (!overwrite) {
-                throw new Exception("cannot overwrite [$file] if you really want to write to this file delete it first")
-            }
-            file.delete()
-        }
-        if (document == null) {
-            throw new Exception("cannot write [$file] no documents have been combined")
-        }
-        log.info "writing [$file] size [$size]"
-        if (visitor != null) {
-            document.accept(visitor)   
-            if (visitor.metaClass.respondsTo(visitor, 'postProcessing', Document)) {
-                visitor.postProcessing(document)
-            }
-        }
-        OutputFormat format = OutputFormat.createPrettyPrint()
-        format.setEncoding(encoding == null ? ENCODING : encoding)
-        format.setIndentSize(0)
-        file.withOutputStream { bos ->
-            XMLWriter xw = new XMLWriter(bos, format)
-            xw.write(document)
-            xw.close()
+        if (file.isFile()) {
+            add(new XmlParser().parse(file))
         }
     }
-    
-    String toString () {
-        if (document == null) {
-            throw new Exception("document is null")
+
+    void add (Node node) {
+        if (root == null) {
+            root = node
+            children = root.children()
         }
-        OutputFormat format = OutputFormat.createPrettyPrint()
-        // StringWriter uses StringBuffer. String Buffer default size is 16 which is a bit small.
-        // Try to guestimate a good value for the initial string size.
-        StringWriter sw = new StringWriter(255 + size)
-        XMLWriter xw = new XMLWriter(sw, format)
-        xw.write(document)
-        return sw.toString()
+        else {
+            node.children().each { children.add(children.size(), it) }
+        }
     }
 
-    void reset () {
-        document = null
-        encoding = null
+    void write (File file) {
+        if (root == null) {
+            throw new Exception("nothing available to write")
+        }
+        log.info "writing to [$file]"
+        file.withWriter(encoding) { wtr ->
+            wtr << "<?xml version=\"1.0\" encoding=\"${encoding}\" ?>\n"
+            if (dtd) {
+                wtr << "<!DOCTYPE import SYSTEM \"${dtd}\">\n"
+            }
+            def xnp = new XmlNodePrinter(new PrintWriter(wtr))
+            xnp.preserveWhitespace = true
+            xnp.print(root)
+        }
         root = null
-        files = []
-        size = 0
-        added = 0
-    }
-
-    void deleteAddedFiles () {
-        files.each { it.delete() }
+        children = null
     }
 
 }
